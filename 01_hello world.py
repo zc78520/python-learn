7ea96ca2b63711bc3dfbaf148f236626d9e7e285
zc78520 <13810833645@163.com>
2018-06-02 09:50:22 +0800

    test
    learn python

---
+++ 01_hello world.py
@@ -0,0 +1,6 @@
+# Filename:hello world.py
+
+print('hello world! zhangchao') #print out a string
+
+a=100;b=200 #many line write on a line with ;
+print(a+b)
---
+++ 02_input.py
@@ -0,0 +1,5 @@
+#!C:\Users\huiqi\AppData\Local\Programs\Python\Python36-32
+
+name=input('please input your name')
+
+print('hello,'name) #use input to input your name,then print it
