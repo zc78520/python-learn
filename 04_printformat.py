cd8930ed2b5b4aa3d953e25a6594c7cda9bfb047
zc78520 <13810833645@163.com>
2018-06-02 10:19:35 +0800

    readme
    learn python

---
+++ 04_printformat.py
@@ -0,0 +1,19 @@
+#!C:\Users\huiqi\AppData\Local\Programs\Python\Python36-32
+#filename:printformat.py
+
+print('I\'m ok')
+print('I\'m leaning\npython')
+print('\\n\\')
+print('\\\t\\')
+print(r'\\\t\\')
+
+print('''line1
+line2
+line3''')
+
+print(r'''hello,\nworld''')
+
+print(True)
+print(True and false)
+print(5>3)
+print(not True)
