8cf63d0afcccb2f43daf703623fca48673b69318
zc78520 <13810833645@163.com>
2018-06-02 09:56:52 +0800

    Revert "test"
    This reverts commit 7ea96ca2b63711bc3dfbaf148f236626d9e7e285.

--- 02_input.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\huiqi\AppData\Local\Programs\Python\Python36-32
-
-name=input('please input your name')
-
-print('hello,'name) #use input to input your name,then print it
--- 03_indentation.py
+++
@@ -1,9 +0,0 @@
-#!C:\Users\huiqi\AppData\Local\Programs\Python\Python36-32
-#Filename:indentation.py
-
-a=100
-if a>=0:
-   print(a)
-else:
-   print(-a)
-   
